45e41ccefe139d33c5e5a3e6f77acd284a5eba30
dj19910501 <dengj1991@hotmail.com>
2019-05-13 23:45:37 -0500

    Fixed loading tabs

--- src/scripts/app.jsx
+++ src/scripts/app.jsx
@@ -12,34 +12,71 @@ import './app.css';
 class App extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            currentPage: 'home'
+        };
     }
 
     render() {
+        const { currentPage } = this.state;
+
         return (
             <div className='main-container'>
                 <Router>
                     <div className='nav-container'>
-                        <NavLink to='/' isActive={() => window.location.pathname === '/'}>Home</NavLink>
-                        <NavLink to='/research'>Research</NavLink>
-                        <NavLink to='/teaching'>Teaching</NavLink>
-                        {/*<NavLink to='/experience'>Experience</NavLink>*/}
-                        <NavLink to='/cv'>CV</NavLink>
+                        <a href='#' onClick={() => this.setState({ currentPage: 'home' })} className={currentPage == 'home' ? 'active' : ''}>Home</a>
+                        <a href='#' onClick={() => this.setState({ currentPage: 'research' })} className={currentPage == 'research' ? 'active' : ''}>Research</a>
+                        <a href='#' onClick={() => this.setState({ currentPage: 'teaching' })} className={currentPage == 'teaching' ? 'active' : ''}>Teaching</a>
+                        <a href='#' onClick={() => this.setState({ currentPage: 'cv' })} className={currentPage == 'cv' ? 'active' : ''}>CV</a>
                     </div>
                     <div className='content-container'>
                         <div className='left-container inline-top'>
                             <Avatar />
                         </div>
                         <div className='right-container inline-top'>
-                            <Route exact path='/' component={MyInfo} />
-                            <Route path='/research' component={Research} />
-                            <Route path='/teaching' component={Teaching} />
-                            {/*<Route path='/experience' component={Experience} />*/}
-                            <Route path='/cv' component={CV} />
+                            {
+                                currentPage == 'research' ? <Research />
+                                    : currentPage == 'teaching' ? <Teaching />
+                                        : currentPage == 'cv' ? <CV /> : <MyInfo />
+                            }
                         </div>
                     </div>
                 </Router>
             </div>
         );
+        /*
+            return (
+                <div className='main-container'>
+                    <Router>
+                        <div className='nav-container'>
+                            <a >Home</a>
+                            <NavLink to='/' isActive={() => window.location.pathname === '/'}>Home</NavLink>
+                            <NavLink to='/research'>Research</NavLink>
+                            <NavLink to='/teaching'>Teaching</NavLink>
+                            {<NavLink to='/experience'>Experience</NavLink>}
+                            <NavLink to='/cv'>CV</NavLink>
+                        </div>
+                        <div className='content-container'>
+                            <div className='left-container inline-top'>
+                                <Avatar />
+                            </div>
+                            <div className='right-container inline-top'>
+                                <Route exact path='/' component={MyInfo} />
+                                <Route path='/research' component={Research} />
+                                <Route path='/teaching' component={Teaching} />
+                                {<Route path='/experience' component={Experience} />}
+                                <Route path='/cv' component={CV} />
+                            </div>
+                        </div>
+                    </Router>
+                    <Route exact path='/' component={MyInfo} />
+                    <Route path='/research' component={Research} />
+                    <Route path='/teaching' component={Teaching} />
+                    {<Route path='/experience' component={Experience} />}
+                    <Route path='/cv' component={CV} />
+                </div>
+            );
+        */
     }
 }
 
