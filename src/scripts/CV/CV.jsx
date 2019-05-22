e5abac7c90b90aecea84c851f417abcc2ffecb2d
Jian Deng <jdeng@hive9.com>
2019-05-22 17:41:49 -0500

    Remove number

--- src/scripts/CV/CV.jsx
+++ src/scripts/CV/CV.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export default class CV extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
     }
 
@@ -21,65 +21,65 @@ export default class CV extends React.Component {
                     fontWeight: 'bold'
                 }}>Li (Kelly) Kang</div>
                 <div className='text-align-center'>2317 Speedway, GDC 7.416b, Austin, TX 78712</div>
-                <div className='text-align-center'>(765)421-5556 likang@utexas.edu</div>
+                <div className='text-align-center'>likang@utexas.edu</div>
                 {/* Education */}
-                <SectionTitle title='EDUCATION'/>
-                <SubSectionTitle title='PhD in Statistics and Data Science' date='May 2020'/>
+                <SectionTitle title='EDUCATION' />
+                <SubSectionTitle title='PhD in Statistics and Data Science' date='May 2020' />
                 <div>University of Texas at Austin</div>
-                <SimpleDescription text='Thesis: Bayesian parametric modeling of Skewness and Kurtosis'/>
-                <SimpleDescription text='Committee: Stephen Walker, Paul Damien, Mingyuan Zhou'/>
-                <SubSectionTitle title='BS in Actuarial Science, Mathematics, Applied Statistics' date='May 2015'/>
+                <SimpleDescription text='Thesis: Bayesian parametric modeling of Skewness and Kurtosis' />
+                <SimpleDescription text='Committee: Stephen Walker, Paul Damien, Mingyuan Zhou' />
+                <SubSectionTitle title='BS in Actuarial Science, Mathematics, Applied Statistics' date='May 2015' />
                 <div>Purdue University, West Lafayette, IN</div>
-                <SimpleDescription text='Graduated with Distinction'/>
+                <SimpleDescription text='Graduated with Distinction' />
                 {/* Teaching & Advising */}
-                <SectionTitle title='TEACHING & ADVISING'/>
-                <SubSectionTitle title='Assistant Instructor, Introduction to statistics in healthcare' date='Aug 2017-Dec 2017'/>
+                <SectionTitle title='TEACHING & ADVISING' />
+                <SubSectionTitle title='Assistant Instructor, Introduction to statistics in healthcare' date='Aug 2017-Dec 2017' />
                 <div>Department of Statistics and Data Science, UT Austin</div>
                 <BulletPoints points={[
                     'Led lecture sessions twice per week (100+ students in pharmacy, biology, liberal arts, etc.)',
                     'Designed course materials including lecture notes, study problems and exams',
                     'Measured students’ study performance through in-class activities'
-                ]}/>
-                <SubSectionTitle title='Teaching Assistant' date='Aug 2015 – present'/>
+                ]} />
+                <SubSectionTitle title='Teaching Assistant' date='Aug 2015 – present' />
                 <div>Department of Statistics and Data Science, UT Austin</div>
                 <BulletPoints points={[
                     'Taught lab sessions for R and Microsoft Excel',
                     'Held weekly office hours to answer students’ questions',
                     'Graded course assessments to ensure students understood material and stayed on track'
-                ]}/>
+                ]} />
                 {/* RESEARCH EXPERIENCE */}
-                <SectionTitle title='RESEARCH EXPERIENCE'/>
-                <SubSectionTitle title='A new class of unimodal, asymmetric, heavy-tailed densities' date='Jul 2017 - Jun 2018'/>
+                <SectionTitle title='RESEARCH EXPERIENCE' />
+                <SubSectionTitle title='A new class of unimodal, asymmetric, heavy-tailed densities' date='Jul 2017 - Jun 2018' />
                 <BulletPoints points={[
                     'Constructed a new class of densities to better capture the skewness and heavy tails that commonly exhibits in financial datasets (Time series modeling)',
                     'Performed linear regression, dynamic linear model, and GARCH model on S&P 500 Index data',
                     'Performed simulation analysis using the techniques of Subsampling'
-                ]}/>
-                <SubSectionTitle title='Multivariate generalization on Gamma mixture of Uniforms' date='May 2018 – Present'/>
+                ]} />
+                <SubSectionTitle title='Multivariate generalization on Gamma mixture of Uniforms' date='May 2018 – Present' />
                 <BulletPoints points={[
                     'Used Copulas to capture the dependence of multi-dimensional skewed and heavy tailed data',
                     'Performed Bayesian MCMC sampling methods to estimate the parameters with uncertainty included'
-                ]}/>
-                <SubSectionTitle title='Bayesian skew mixture models for loss distribution' date='May 2018 – Present'/>
+                ]} />
+                <SubSectionTitle title='Bayesian skew mixture models for loss distribution' date='May 2018 – Present' />
                 <BulletPoints points={[
                     'Proposed a finite skew mixture model that provides a better characterization of insurance data, and could be easily generalized to other datasets',
                     'Developed a Hierarchical structure to make the statistics model easier to understand',
                     'Performed Bayesian MCMC sampling methods in R to better predict future losses'
-                ]}/>
+                ]} />
                 {/* HONORS AND AWARDS */}
-                <SectionTitle title='HONORS AND AWARDS'/>
-                <SubSectionTitle title='SBSS Student Paper Competition winner' date='2019' noBold={true}/>
+                <SectionTitle title='HONORS AND AWARDS' />
+                <SubSectionTitle title='SBSS Student Paper Competition winner' date='2019' noBold={true} />
 
-                <SubSectionTitle title='McCombs School of Business Fellowship for Research Excellence' date='2017 - 2018' noBold={true}/>
-                <SimpleDescription text='University of Texas at Austin'/>
+                <SubSectionTitle title='McCombs School of Business Fellowship for Research Excellence' date='2017 - 2018' noBold={true} />
+                <SimpleDescription text='University of Texas at Austin' />
 
-                <SubSectionTitle title='Outstanding Junior in College of Science' date='2014' noBold={true}/>
-                <SimpleDescription text='Purdue University, West Lafayette, IN'/>
+                <SubSectionTitle title='Outstanding Junior in College of Science' date='2014' noBold={true} />
+                <SimpleDescription text='Purdue University, West Lafayette, IN' />
 
-                <SubSectionTitle title='Dean’s List and Semester Honors' date='2011 - 2015' noBold={true}/>
-                <SimpleDescription text='Purdue University, West Lafayette, IN'/>
+                <SubSectionTitle title='Dean’s List and Semester Honors' date='2011 - 2015' noBold={true} />
+                <SimpleDescription text='Purdue University, West Lafayette, IN' />
                 {/* REFERENCES */}
-                <SectionTitle title='REFERENCES'/>
+                <SectionTitle title='REFERENCES' />
                 <SubSectionTitle title='Stephen Walker, Professor and Graduate Advisor' />
                 <SimpleDescription text='Department of Statistics and Data Sciences (SDS)' />
                 <SimpleDescription text='Department of Mathematics' />
@@ -87,7 +87,7 @@ export default class CV extends React.Component {
                 <SimpleDescription text='Email: s.g.walker@math.utexas.edu' />
 
                 <SubSectionTitle title='Paul Damien, Professor' />
-                <SimpleDescription text='Department of Information, Risk, and Operations Management'/>
+                <SimpleDescription text='Department of Information, Risk, and Operations Management' />
                 <SimpleDescription text='Department of Finance' />
                 <SimpleDescription text='University of Texas at Austin' />
                 <SimpleDescription text='Email: paul.damien@mccombs.utexas.edu' />
@@ -114,7 +114,7 @@ function SectionTitle(props) {
 }
 
 function SubSectionTitle(props) {
-const { title = '', date = '', noBold } = props;
+    const { title = '', date = '', noBold } = props;
     const subSectionTitleStyle = { fontSize: '16px', marginTop: 5, fontWeight: noBold ? 'normal' : 'bold' };
     const subSectionTitleLeftStyle = { fontStyle: 'italic', width: 'calc(100% - 170px)' };
     const subSectionTitleRightStyle = { width: 170 };
